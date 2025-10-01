30db86a0c5274a23bcce054df2f012ca0026aca7
Christopher Salmi <118998097+Chris58903@users.noreply.github.com>
2025-09-30 22:47:05 -0500

    Added add todo funct

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
+  // Fetch todos on mount
   useEffect(() => {
     fetch("http://localhost:3001/todos")
       .then(res => res.json())
@@ -11,9 +13,44 @@ function App() {
       .catch(err => console.error("Error fetching todos:", err));
   }, []);
 
+  // Add a new todo
+  const addTodo = () => {
+    if (!newTodo) return;
+
+    fetch("http://localhost:3001/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text: newTodo })
+    })
+      .then(res => res.json())
+      .then(todo => {
+        setTodos([...todos, todo]); 
+        setNewTodo("");             
+      })
+      .catch(err => console.error("Error adding todo:", err));
+  };
+
   return (
     <div className="App">
       <h1>My Todo List</h1>
+
+      {/* Add Todo Form */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo();
+        }}
+      >
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      {/* Todo List */}
       <ul>
         {todos.map(todo => (
           <li key={todo.id}>
@@ -22,7 +59,7 @@ function App() {
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default App;
